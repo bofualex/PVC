845cc9f42d93d444efb75696d80b296a1bf83fbb
Alex Bofu <A70534@eon.com>
2023-04-10 12:01:54 +0300

    Initial Commit

---
+++ PVCWindowsGenerator/PVCWindowsGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PVCWindowsGeneratorApp.swift
+//  PVCWindowsGenerator
+//
+//  Created by Alex Bofu on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PVCWindowsGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
